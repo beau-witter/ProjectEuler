7bdcc35574600403d9d856fd1a7fffac9d003986
Beau Witter <beau.witter@gmail.com>
2018-02-17 11:27:28 -0800

    Got babel and mocha up and running for testing

---
+++ Tests/Problem12/index.test.js
@@ -0,0 +1,11 @@
+import {assert} from "chai";
+
+describe("The test", () => {
+    it("passes as expected", () => {
+        assert.isTrue(true);
+    });
+
+    it("fails as expected", () => {
+        assert.isTrue(true);
+    });
+});
